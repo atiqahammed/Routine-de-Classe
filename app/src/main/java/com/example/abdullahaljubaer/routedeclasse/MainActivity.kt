68b889e79f1f959e2226c202884ed2b21ed9acef
aljubaer <bsse0812@iit.du.ac.bd>
2018-11-06 00:17:40 +0600

    Welcome screen added

--- app/src/main/java/com/example/abdullahaljubaer/routedeclasse/MainActivity.kt
+++ app/src/main/java/com/example/abdullahaljubaer/routedeclasse/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.abdullahaljubaer.routedeclasse
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
