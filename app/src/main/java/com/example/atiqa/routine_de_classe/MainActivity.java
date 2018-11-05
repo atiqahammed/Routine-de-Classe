6052702401482a1ed751c951b575e34bc375100c
atiqahammed <atiqahammedshamim@gmail.com>
2018-11-05 20:03:09 +0600

    new activity added in master brunch

--- app/src/main/java/com/example/atiqa/routine_de_classe/MainActivity.java
+++ app/src/main/java/com/example/atiqa/routine_de_classe/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.atiqa.routine_de_classe;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASh_TIME_OUT = 4000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASh_TIME_OUT);
     }
 }
